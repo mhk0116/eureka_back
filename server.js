76826c4875515453bd9912aeea4775fe8915fb44
Minhwan Kim <mhk0116@gmail.com>
2020-05-19 18:35:21 +0900

    Apply Elasticsearch

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const request = require("request");
+var elasticsearch = require("elasticsearch");
+
 const app = express();
 const port = process.env.PORT || 5000;
-var elasticsearch = require("elasticsearch");
 var client = elasticsearch.Client({
   host: "localhost:9200",
 });
@@ -12,18 +12,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/api/data", (req, res) => {
-  //     request(
-  //     "http://localhost:9200/classes/_search",
-  //     (err, res) => {
-  //       res.json(body);
-  //     }
-  //   );
   client
     .search({
-      index: "classes",
+      index: "data",
       body: {
         query: {
-          match: { title: "" },
+          match_all: {},
         },
       },
     })
